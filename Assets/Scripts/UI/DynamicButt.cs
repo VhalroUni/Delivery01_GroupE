077fdd8e5ccbb4988f22797dc7bb725e741a6622
ritchie13jr <xritchie13@gmail.com>
2025-03-24 17:38:43 +0100

    Dynamic button
    Works with name of gameObject: name convention: FunctionButt

---
+++ Assets/Scripts/UI/DynamicButt.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DynamicButt : MonoBehaviour
+{
+    private Button button;
+
+    private void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(HandleButtonClick);
+    }
+
+    private void HandleButtonClick() 
+    {
+        SceneChanger sc = FindAnyObjectByType<SceneChanger>();
+
+        switch (gameObject.name) 
+        {
+            case "StartButt": 
+            sc.LoadGame();
+            break;
+
+            case "ExitButt":
+            sc.QuitGame();
+            break;
+
+            case "TitleButt":
+            sc.LoadMenu();
+            break;
+
+            default:
+            Debug.Log("This Button does nothing");
+            break;
+        }
+    }
+}
